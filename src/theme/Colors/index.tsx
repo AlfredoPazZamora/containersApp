2cebb12f6ce5a5e1489adf49f51218222402e875
Alfredo Paz Zamora <apaz0@ucol.mx>
2021-11-23 22:35:41 -0600

    Archivo de colores

---
+++ src/theme/Colors/index.tsx
@@ -0,0 +1,12 @@
+export const colors = {
+    primary: '#0466C8',
+    secondary: '#FB8500',
+    black: '#000',
+    white: '#FFF',
+    red: '#D90429',
+    green: '#008000',
+    blue_001: '#001233',
+    gray_334: '#33415C',
+    gray_979: '#979DAC',
+    gray_f2f: '#F2F2F2',
+}
